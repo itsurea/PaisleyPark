843472286a7cae58c45043b1b3930ec1818fe6eb
itsurea <admin@itsurea.net>
2020-01-22 12:21:38 +0900

    kr server update

--- PaisleyPark/ViewModels/MainWindowViewModel.cs
+++ PaisleyPark/ViewModels/MainWindowViewModel.cs
@@ -39,7 +39,7 @@ namespace PaisleyPark.ViewModels
 		private readonly Version CurrentVersion;
 		private string GameVersion;
 		public string DiscordUri { get; private set; } = "https://discord.gg/hq3DnBa";
-		private static readonly Uri OffsetUrl = new Uri("https://raw.githubusercontent.com/LeonBlade/PaisleyPark/master/Offsets/");
+		private static readonly Uri OffsetUrl = new Uri("https://raw.githubusercontent.com/itsurea/PaisleyPark/master/Offsets/");
 
 #pragma warning disable IDE1006 // Naming Styles
 
